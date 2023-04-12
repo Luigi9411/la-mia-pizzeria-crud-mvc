94bd80cf7fa8d5dccfab1b92d1d802147c9c0595
Luigi9411 <luigi.sciavicco@gmail.com>
2023-04-12 14:13:18 +0200

    Aggiungere i file di progetto.

---
+++ la-mia-pizzeria-static/Controllers/PizzaController .cs
@@ -0,0 +1,59 @@
+﻿using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        private readonly ILogger<PizzaController> _logger;
+        private readonly PizzaContext _context;
+
+        public PizzaController(ILogger<PizzaController> logger, PizzaContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+
+            var pizzas = _context.Pizzas.ToArray();
+
+            return View(pizzas);
+        }
+
+        public IActionResult Detail(int id)
+        {
+
+            var pizza = _context.Pizzas.Find(id);
+
+            if (pizza is null)
+            {
+                return NotFound($"L'id {id} della pizza non è stato trovato");
+            }
+
+            return View(pizza);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza pizza)
+        {
+            if (!ModelState.IsValid)
+            {
+               return View(pizza);
+            }
+
+            _context.Pizzas.Add(pizza);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ la-mia-pizzeria-static/Models/PizzaContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class PizzaContext : DbContext
+    {
+        public PizzaContext(DbContextOptions<PizzaContext> options) : base(options) { }
+        public DbSet<Pizza> Pizzas { get; set; }
+
+        public void Seed()
+        {
+
+            if (!Pizzas.Any())
+            {
+                var seed = new Pizza[]
+                {
+            new Pizza
+            {
+                Name = "Pizza Margherita",
+                Description = "Pizza classica dal gusto inconfondibile.",
+                Image = "img/margherita-50kalo.jpg",
+                Price = 4,
+            },
+            new Pizza
+            {
+                Name = "Americana",
+                Description = "Pizza che De Sica definirebbe con: 'Che è sta cafonata'.",
+                Image = "img/americana.jpg",
+                Price = 5,
+            },
+            new Pizza
+            {
+                Name = "Diavola",
+                Description = "Pizza leggera ma quel pizzico in più.",
+                Image = "img/diavola.jpg",
+                Price = 4.5,
+            },
+            new Pizza
+            {
+                Name = "Capricciosa",
+                Description = "Pizza amata dal ristoratore. Gli permette di liberarsi dei condimenti",
+                Image = "img/Capricciosa.jpg",
+                Price = 6.5,
+            }
+            };
+
+                Pizzas.AddRange(seed);
+
+                SaveChanges();
+            }
+        }
+    }
+}
+
